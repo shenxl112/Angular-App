0b79f543720205eb9ae40175bfb32405d040452a
shenxl112 <shenxl1102@gmail.com>
2019-06-10 13:56:17 -0400

    update selecotr and template

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,8 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
-  templateUrl: './servers.component.html',
+  selector: 'app-servers',      //element
+  // selector: '[app-servers]', //attribute
+  // selector: '.app-servers',  //class
+  
+  template: `
+    <app-server></app-server>
+    <app-server></app-server>`,
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
