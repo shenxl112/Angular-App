fe71d025a222d901ca1b18a0524bd25dda46c1ce
shenxl112 <shenxl1102@gmail.com>
2019-06-11 16:56:14 -0400

    Update DataBinding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -5,16 +5,32 @@ import { Component, OnInit } from '@angular/core';
   // selector: '[app-servers]', //attribute
   // selector: '.app-servers',  //class
   
-  template: `
-    <app-server></app-server>
-    <app-server></app-server>`,
+  // template: `
+  //   <app-server></app-server>
+  //   <app-server></app-server>`,
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = '';
 
-  constructor() { }
+  constructor() { 
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
 
   ngOnInit() {
   }
 
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created';
+  }
+
+  onUpdateServerName(event: Event){
+    // console.log(event);
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
 }
